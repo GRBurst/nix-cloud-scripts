a8387c659caa8936ede2795eec07a3fcacd3f8b9
GRBurst <GRBurst@protonmail.com>
2022-09-01 15:03:31 +0200

    feat: add simple interactive terraform taint/untaint scripts
    Signed-off-by: GRBurst <GRBurst@protonmail.com>

---
+++ terraform/tf-taint-resource.sh
@@ -0,0 +1,71 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash
+#! nix-shell -p terraform aws-vault
+#! nix-shell -p fzf
+##! nix-shell --pure
+##! nix-shell --keep AWS_PROFILE --keep DEBUG
+# remove one # for the 2 shebangs above during devlopment of the script.
+
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail
+set -Eeuo pipefail
+
+# source lib.sh
+source "$(dirname "${BASH_SOURCE[0]}")/../script-cook/lib.sh"
+
+# This will contain the resulting parameters of your command
+declare -a params
+
+
+############################################
+########## BEGIN OF CUSTOMISATION ##########
+############################################
+
+# Configure your parameters here
+declare -A options=()
+
+# Define your usage and help message here
+usage() (
+    local script_name="${0##*/}"
+    cat <<-USAGE
+Choose an instance interactively and taint it.
+
+
+Usage and Examples
+---------
+
+- Run:
+    $script_name
+
+
+$(_generate_usage options)
+USAGE
+)
+
+# Put your script logic here
+run() (
+
+    terraform taint $(terraform state list | fzf)
+)
+
+
+############################################
+########### END OF CUSTOMISATION ###########
+############################################
+
+# This is the base frame and it shouldn't be necessary to touch it
+self() (
+    declare -a args=( "$@" )
+    if [[ "${1:-}" == "help" ]] || [[ "${1:-}" == "--help" ]]; then
+        usage
+    elif (check_requirements options args); then
+
+        process_args options args params || _print_debug "Couldn't process args, terminated with $?"
+
+        run
+    else
+        _print_debug "Requirements not met"
+    fi
+
+)
+
+self "$@"
